71ec40690ec07cf781dc3b1e60928a9f411555b3
Alberto Buffolino <a.buffolino@gmail.com>
2018-11-27 21:06:52 +0100

    Added onInstall check to delete old settings.ini file.

---
+++ addon/installTasks.py
@@ -0,0 +1,23 @@
+import addonHandler
+import gui
+import wx
+import os
+
+addonHandler.initTranslation()
+
+def onInstall():
+	for addon in addonHandler.getAvailableAddons():
+		if addon.name == "columnsReview":
+			iniFile = os.path.join(addon.path, "globalPlugins", "settings.ini").decode("mbcs")
+			if os.path.isfile(iniFile):
+				gui.messageBox(
+					# Translators: the label of a message box dialog.
+					_("Previous add-on settings will be lost. Please configure it again, from NVDA preferences."),
+					# Translators: the title of a message box dialog.
+					_("Add-on settings reset"),
+					wx.OK|wx.ICON_WARNING)
+				try:
+					os.remove(iniFile)
+				except:
+					pass
+			break
