baa2e46ac9acb93d716693836935ce0cc317cc61
serrebidev <serrebi101@gmail.com>
2026-05-25 18:20:59 -0700

    Use buildVersion for NVDA version compatibility

--- addon/globalPlugins/columnsReview/compat.py
+++ addon/globalPlugins/columnsReview/compat.py
@@ -2,7 +2,11 @@
 # Provides various stuff used to preserve compatibility with older releases of NVDA.
 
 import controlTypes
-from versionInfo import *
+
+try:
+	from buildVersion import version_year, version_major, version_minor
+except ImportError:
+	from versionInfo import version_year, version_major, version_minor
 
 currentVersion = (version_year, version_major, version_minor)
 
