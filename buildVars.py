177e41e8b95d2a633fcad93622ca9a64b4ae9ad3
ABuffEr <a.buffolino@gmail.com>
2026-06-01 14:58:27 +0200

    Bumps version to 5.6.0

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""A better experience with lists"""),
 	# version
-	addon_version="5.5.0",
+	addon_version="5.6.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""2026.x compatibility."""),
